79ac8578e3ca6f6affc94a4f7060377dcc4a7b11
Nguyễn Thị Ánh Trúc <2212479@dlu.edu.vn>
2025-10-26 01:21:30 +0700

    Delete 2212479_NTATruc_MathQuizProject_MTK directory

--- 2212479_NTATruc_MathQuizProject_MTK/src/main/java/com/mycompany/mathgame/strategy/question/QuestionStrategy.java
+++
@@ -1,4 +0,0 @@
-package com.mycompany.mathgame.strategy.question;
-import com.mycompany.mathgame.core.Question;
-import java.util.Random;
-public interface QuestionStrategy { Question next(Random rnd, int level); }
